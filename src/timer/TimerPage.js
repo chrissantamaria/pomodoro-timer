8464bd990595504eec3580fef057637a5a943d56
Chris Santamaria <chris@santamaria.me>
2019-05-28 15:40:24 -0400

    Added material-ui and styled TimerPage

--- src/timer/TimerPage.js
+++ src/timer/TimerPage.js
@@ -1,10 +1,23 @@
 import React from "react";
-import "./TimerPage.css";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+
+import useStyles from "./TimerPageStyles";
 
 export default function TimerPage() {
+  const classes = useStyles();
+
   return (
-    <div className="root">
-      <h1>Timer page</h1>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography className={classes.title} variant="h4" align="center">
+            Timer
+          </Typography>
+        </Paper>
+      </main>
+    </React.Fragment>
   );
 }
---
+++ src/timer/TimerPageStyles.js
@@ -0,0 +1,29 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(4))]: {
+      width: 900,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(6))]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      padding: theme.spacing(3)
+    }
+  },
+  title: {
+    paddingBottom: 30
+  }
+}));
+
+export default useStyles;
