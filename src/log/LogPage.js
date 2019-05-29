d910e27f5b2c2788d2d3625a5a510c4cf513242e
Chris Santamaria <chris@santamaria.me>
2019-05-29 15:56:41 -0400

    Added feature log (#9)
    Fetches data from Firebase and allows for editing/deleting rows

---
+++ src/log/LogPage.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import firebase from "../firebase/firebase";
+
+import Table from "./Table";
+import useStyles from "./LogPageStyles";
+
+export default function LogPage() {
+  const classes = useStyles();
+
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    console.log("Setting up firebase listener");
+    const sessionsRef = firebase.database().ref("sessions");
+
+    sessionsRef.on("value", snap => {
+      console.log("snap.val():", snap.val());
+      // Mapping to an array structure with a "key" property on each object
+      const snapSessions = !snap.val()
+        ? []
+        : Object.keys(snap.val()).map(key => ({
+            ...snap.val()[key],
+            key,
+            // Mapping each incoming timestamp into a Date object
+            date: new Date(snap.val()[key].date)
+          }));
+      console.log("snapSessions:", snapSessions);
+      setSessions(snapSessions);
+    });
+  }, []);
+
+  const updateSession = ({ session, key }) => {
+    firebase
+      .database()
+      .ref(`sessions/${key}`)
+      .update(session);
+    console.log(`Updated session id ${key}`);
+  };
+  const removeSession = key => {
+    firebase
+      .database()
+      .ref(`sessions/${key}`)
+      .remove();
+    console.log(`Deleted session id ${key}`);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main className={classes.layout}>
+        <div className={classes.content}>
+          <Table
+            sessions={sessions}
+            updateSession={updateSession}
+            removeSession={removeSession}
+          />
+        </div>
+      </main>
+    </React.Fragment>
+  );
+}
---
+++ src/log/Table.js
@@ -0,0 +1,33 @@
+import React from "react";
+import MaterialTable from "material-table";
+
+export default function Table(props) {
+  const { sessions, updateSession, removeSession } = props;
+  return (
+    <div style={{ maxWidth: "100%" }}>
+      <MaterialTable
+        data={sessions}
+        columns={[
+          { title: "Type", field: "sessionType" },
+          { title: "Desciption", field: "description" },
+          { title: "Date", field: "date", type: "date", editable: "never" },
+          { title: "Time", field: "date", type: "time", editable: "never" }
+        ]}
+        title="Timer logs"
+        options={{ actionsColumnIndex: -1 }}
+        editable={{
+          onRowUpdate: async data => {
+            // Stripping "tableData" property from oldSession that comes from material-table
+            const { tableData, key, ...session } = data;
+            updateSession({ session, key });
+            return;
+          },
+          onRowDelete: async data => {
+            removeSession(data.key);
+            return;
+          }
+        }}
+      />
+    </div>
+  );
+}
