b3291f6dcd5ef02ff2faaf102622b548134ca226
samhecht <34660188+samhecht@users.noreply.github.com>
2019-05-29 10:50:34 -0400

    Feature set session (#4)
    * committing before pull
    
    * break and work toggle

--- src/timer/TimerPage.js
+++ src/timer/TimerPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Paper from "@material-ui/core/Paper";
 
@@ -9,13 +9,18 @@ import useStyles from "./TimerPageStyles";
 export default function TimerPage() {
   const classes = useStyles();
 
+  const [sessionType, setSessionType] = useState('Work');
+
+  const changeParentSession = (sess) => {
+    setSessionType(sess);
+  }
   return (
     <React.Fragment>
       <CssBaseline />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
-          <Timer />
-          <SessionForm />
+          <Timer sessionType={sessionType}/>
+          <SessionForm changeParentSession={changeParentSession}/>
         </Paper>
       </main>
     </React.Fragment>
