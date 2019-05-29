2f1b9ad140106983d1c9abae3763f54b910ea86d
samhecht <34660188+samhecht@users.noreply.github.com>
2019-05-29 15:47:43 -0400

    Feature signin redirect (#8)
    * committing before pull
    
    * added navigation

--- src/timer/TimerPage.js
+++ src/timer/TimerPage.js
@@ -5,6 +5,7 @@ import Paper from "@material-ui/core/Paper";
 import Timer from "./Timer";
 import SessionForm from "./SessionForm";
 import useStyles from "./TimerPageStyles";
+import { Redirect } from "react-router-dom";
 
 export default function TimerPage() {
   const classes = useStyles();
@@ -14,6 +15,10 @@ export default function TimerPage() {
   const changeParentSession = sess => {
     setSessionType(sess);
   };
+
+  if (!sessionStorage.getItem("userId")) {
+    return <Redirect to="/signIn" />
+  }
   
   return (
     <React.Fragment>
