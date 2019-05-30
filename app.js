edd11f741531b01cd341b6aeefc01210d3b8dbff
Chris Santamaria <chris@santamaria.me>
2019-05-30 13:27:50 -0400

    Removed cors from server, added CRA proxy

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const axios = require('axios');
-const port = 9000;
 
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://opentdb.com/api.php");
-    return next();
-});
+const axios = require("axios");
 
 app.get("/break", (req, res) => {
-    const url = "https://opentdb.com/api.php?amount=1000";
-    axios
-        .get(url)
-        .then(response => {
-            // send back the array of questions
-            res.send(response.data.results);
-        })
-        .catch(err => {
-            console.log(`Couldn't get data from trivia api: ${err}`);
-        });
-    
+  const url = "https://opentdb.com/api.php?amount=1000";
+  axios
+    .get(url)
+    .then(response => {
+      // send back the array of questions
+      res.send(response.data.results);
+    })
+    .catch(err => {
+      console.log(`Couldn't get data from trivia api: ${err}`);
+    });
 });
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+const port = 9000;
+app.listen(port, () => console.log(`listening on port ${port}`));
